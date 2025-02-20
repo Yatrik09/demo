0a758289a8de7e6144a90a3f1b4536828862eda8
Yatrik <yatrikpatel1331@gmail.com>
2025-02-20 15:16:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./component/About";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import{BrowserRouter,Routes,Route} from "react-router-dom";
+
+function App (){
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App;
---
+++ src/component/About.jsx
@@ -0,0 +1,9 @@
+function About(){
+    return (
+        <h1>
+            Welcome to our About us 
+        </h1>
+    )
+}
+
+export default About 
